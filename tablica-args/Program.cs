3379afa564ea55edfa61d99337171c246f68add6
Myasull <adrian_wozniak@poczta.fm>
2019-05-26 15:39:09 +0200

    Initial commit

--- tablica-args/Program.cs
+++ tablica-args/Program.cs
@@ -10,6 +10,13 @@ namespace tablica_args
     {
         static void Main(string[] args)
         {
+            //string name = args[0];
+            //Console.WriteLine("Witaj, " + name);
+
+            foreach (var item in args)
+            {
+                Console.WriteLine("Witaj, " + item);
+            }
         }
     }
 }
